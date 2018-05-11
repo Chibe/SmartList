40c7ee78499692bc70900c424ebb31453a2b23be
Drew <Chibe1313@gmail.com>
2018-05-11 13:16:01 -0400

    Initial Commit

--- SmartList/Resources/Resource.Designer.cs
+++ SmartList/Resources/Resource.Designer.cs
@@ -41,6 +41,28 @@ namespace SmartList
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int CreateListButton = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int CreateNewRecipeButton = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int ExistingLists = 2130968578;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
